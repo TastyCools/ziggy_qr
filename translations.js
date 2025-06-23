103010340c1af0c0cfd7358becac8a7f1a77f78f
Vincent Paquette <vincentpaquette@gmail.com>
2025-06-23 16:41:57 -0400

    moved translations outside

---
+++ translations.js
@@ -0,0 +1,17 @@
+// Translation objects
+const translations = {
+    en: {
+        'page-title': 'ZIGGY',
+        'page-subtitle': 'Am I lost? <b data-i18n="no-answer" class="no-answer">NO</b>',
+        'no-answer': 'NO',
+        'description': 'You can usually find me in this area:',
+        'copyright': '&copy; 2025 Ziggy QR'
+    },
+    fr: {
+        'page-title': 'ZIGGY',
+        'page-subtitle': 'Suis-je perdu ? <b data-i18n="no-answer" class="no-answer">NON</b>',
+        'no-answer': 'NON',
+        'description': 'Vous pouvez habituellement me trouver dans ce secteur:',
+        'copyright': '&copy; 2025 Ziggy QR'
+    }
+}; 
