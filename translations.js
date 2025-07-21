8fa047308d966029a0f2e3df8380057293437bb0
TastyCools <vincentpaquette@gmail.com>
2025-07-20 22:21:59 -0400

    Update translations.js

--- translations.js
+++ translations.js
@@ -2,14 +2,14 @@
 const translations = {
     en: {
         'page-title': 'ZIGGY',
-        'page-subtitle': 'Am I lost? <b data-i18n="no-answer" class="no-answer">NO</b>',
+        'page-subtitle': 'Am I lost? <b data-localize="no-answer" class="no-answer">NO</b>',
         'no-answer': 'NO',
         'description': 'You can usually find me in this area:',
         'copyright': '&copy; 2025 Ziggy QR'
     },
     fr: {
         'page-title': 'ZIGGY',
-        'page-subtitle': 'Suis-je perdu ? <b data-i18n="no-answer" class="no-answer">NON</b>',
+        'page-subtitle': 'Suis-je perdu ? <b data-localize="no-answer" class="no-answer">NON</b>',
         'no-answer': 'NON',
         'description': 'Vous pouvez habituellement me trouver dans ce secteur:',
         'copyright': '&copy; 2025 Ziggy QR'
